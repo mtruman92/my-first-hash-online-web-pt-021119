154723b67849079feed6b486a07bfcf4ff3f7455
Mickae Truman <mtruman@nyit.edu>
2019-02-21 23:26:21 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,6 +37,7 @@ def adding
 #shipping_manifest = {:muskets => 2},{:"gun powder" => 4}
 #shipping_manifest=>{:muskets=> 2, :gun powder=> 4}
 shipping_manifest["muskets"] = 2
+shipping_manifest
   # add 4 gun powder to the shipping_manifest hash below
 
 
